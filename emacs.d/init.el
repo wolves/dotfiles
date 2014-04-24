4830a0323f52c37df73cada5f1b1c86f76e8add8
Christopher Stingl <cjstingl@gmail.com>
2014-04-24 09:59:37 -0400

    [CS] Updating init.el & testing magit w/ this

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,48 +1,50 @@
-;;; init.el --- Proem's configuration entry point.
-;;
-;; Copyright (c) 2014 Christopher Stingl
-;;
-;; Author: Christopher Stingl <cjstingl@gmail.com>
-;; URL: http://cjstingl.com/proem
-;; Version: 0.0.1
-;; Keywords: convenience
-
-;; This file is not part of GNU Emacs.
+;;; init.el --- My configuration entry point.
 
 ;;; Commentary:
 
-;; This file simply sets up the default load path and requires
-;; the various modules defined within Emacs Proem. I also want
-;; to take this moment to give credit and thanks to Bozhidar
-;; Batsov's Prelude <https://github.com/bbatsov/prelude> of
-;; which this is clearly inspired.
-
-;;; License:
-
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License
-;; as published by the Free Software Foundation; either version 3
-;; of the License, or (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;; Boston, MA 02110-1301, USA.
-
 ;;; Code:
-(defvar current-user
-      (getenv
-       (if (equal system-type 'windows-nt) "USERNAME" "USER")))
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
 
-(message "Proem is powering up... Be patient, Master %s!" current-user)
+(require 'flycheck)
+(require 'grizzl)
+(require 'projectile)
 
-(when (version< emacs-version "24.1")
-  (error "Proem requires at least GNU Emacs 24.1, but you're running %s" emacs-version))
+(require 'coffee-mode)
+(require 'elixir-mode)
+(require 'haml-mode)
+(require 'rspec-mode)
+(require 'sass-mode)
+(require 'slim-mode)
 
-;; Always load newest byte code
-(setq load-prefer-newer t)
+;; use zenburn as the default theme
+(load-theme 'zenburn t)
+
+;; hide toolbar
+(tool-bar-mode -1)
+
+;; key mappings
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
+;; Exec Path from Shell
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; Flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; Magit
+;;(setq magit-git-executable "/usr/local/bin/git")
+
+;; Projectile
+(projectile-global-mode)
+(setq projectile-completion-system 'grizzl)
+
+(provide 'init)
+;;; init.el ends here
